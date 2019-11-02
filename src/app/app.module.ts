1114cc58048b43163bccca0a5b84f07e9bf28f45
Kittisak Buaphanna <kurosaki_tisak@hotmail.com>
2019-11-02 23:13:06 +0700

    [Added] - Login, Register, Request-Password, Reset-Password components [Added] - AuthService, AuthGuard providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
+import { APP_BASE_HREF } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
@@ -19,10 +20,28 @@ import {
   NbSidebarModule,
   NbToastrModule,
   NbWindowModule,
+  NbLayoutModule
 } from '@nebular/theme';
+import { environment } from '../environments/environment';
+
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// Auth
+import { AuthGuard } from './auth/auth-guard.service';
+import { AuthService } from './auth/auth-service.service';
+import { LoginComponent } from './auth/login/login.component';
+import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { RequestPasswordComponent } from './auth/request-password/request-password.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent, 
+    LoginComponent, ResetPasswordComponent, RegisterComponent, RequestPasswordComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -37,12 +56,22 @@ import {
     NbDialogModule.forRoot(),
     NbWindowModule.forRoot(),
     NbToastrModule.forRoot(),
+    NbLayoutModule,
     NbChatModule.forRoot({
       messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY',
     }),
     CoreModule.forRoot(),
+
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule
+
   ],
   bootstrap: [AppComponent],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+    AuthGuard, AuthService
+  ],
 })
 export class AppModule {
 }
